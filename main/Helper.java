6b40b1c8144086311123fd4c46190ea47aeaa61c
Nana <stnisrinasalsabila@gmail.com>
2026-03-14 13:57:58 +0700

    Update Helper Class

--- main/Helper.java
+++ main/Helper.java
@@ -15,8 +15,7 @@ public class Helper {
         int len = hex.length();
         byte[] data = new byte[len / 2];
         for (int i = 0; i < len; i += 2) {
-            data[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4)
-                                 + Character.digit(hex.charAt(i+1), 16));
+            data[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4)+ Character.digit(hex.charAt(i+1), 16));
         }
         return data;
     }
