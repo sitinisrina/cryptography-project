6f1abb7834e1f61d780ce939c3381846e593d026
Nana <stnisrinasalsabila@gmail.com>
2026-03-17 08:28:08 +0700

    Ambil PubKey Bob dari file dengan bantuan Helper

--- main/PovAliceasSender.java
+++ main/PovAliceasSender.java
@@ -1,5 +1,22 @@
 import java.util.Scanner;
 
+/*
+TAHAP 1
+alice membangkitkan pasangan kunci RSA dengan HybridRSA_AES.generateRSAKeyPair()
+alice mengambil bob public key dari file "bob_public_key.bin" dengan Helper.loadPublicKey("bob_public_key.bin")
+
+TAHAP 2
+alice membangkitkan kunci sesi AES dengan HybridRSA_AES.generateSessionKey()
+alice mengenkripsi kunci sesi AES dengan kunci publik Bob (65537), HybridRSA_AES.encryptSessionKey(sessionKey, publicKey)
+alice menyimpan encrypted session key, untuk dikirimkan bersamaan dengan cipher text.
+
+TAHAP 3
+alice menginput plaintext (apapun) dari user dengan scanner
+alice mengenkripsi plaintext dengan HybridRSA_AES.encryptMessage(plaintext, sessionKey)
+alice encoding hasil cipher text dan encrypted session key, kemudian dikirim ke Bob.
+
+*/
+
 public class PovAliceasSender {
     
     public static void main(String[] args) {
@@ -8,16 +25,14 @@ public class PovAliceasSender {
         String filePath = scanner.nextLine();
 
         try {
-            // 1. Generate RSA key pair
-            var rsaKeyPair = HybridRSA_AES.generateRSAKeyPair();
-            var publicKey = rsaKeyPair.getPublic();
-            var privateKey = rsaKeyPair.getPrivate();
+            // 1. Take Bob's RSA public key
+            var bobPublicKey = Helper.loadPublicKey("bob_public_key.bin");
 
             // 2. Generate AES session key
             var sessionKey = HybridRSA_AES.generateSessionKey();
 
             // 3. Encrypt AES session key with RSA public key
-            var encryptedSessionKey = HybridRSA_AES.encryptSessionKey(sessionKey, publicKey);
+            var encryptedSessionKey = HybridRSA_AES.encryptSessionKey(sessionKey, bobPublicKey);
 
             // 4. Encrypt file content with AES session key
             var fileContent = Helper.fromFiletoBinary(filePath);
@@ -28,7 +43,6 @@ public class PovAliceasSender {
             Helper.writeBinarytoFile(encryptedFileContent, "encrypted_file.bin");
 
             System.out.println("File berhasil dienkripsi dan disimpan sebagai 'encrypted_session_key.bin' dan 'encrypted_file.bin'.");
-            System.out.println("Public Key RSA (Base64): " + Helper.fromBinaryToBase64(publicKey.getEncoded()));
 
         } catch (Exception e) {
             e.printStackTrace();
